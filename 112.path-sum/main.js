db2d248c94051ed30b333d8c6157e3f872d0454d
GerritV <shigw5420@joyowo.com>
2019-06-21 16:12:05 +0800

    path sum

---
+++ 112.path-sum/main.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode-cn.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (46.05%)
+ * Likes:    146
+ * Dislikes: 0
+ * Total Accepted:    20.1K
+ * Total Submissions: 43.1K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \      \
+ * ⁠       7    2      1
+ * 
+ * 
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) { // 从上到下计算到最后的叶子节点返回总和是不是和 sum 相等
+  if (!root) return false // 最后一个 null 直接返回 false
+
+  if (!root.left && !root.right) {
+    return sum === root.val  // 1 结束条件
+  } else {
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val) // 2 等价关系
+  }
+}
